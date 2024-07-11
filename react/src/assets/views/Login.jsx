b9603f9113b44aeecbb71b7fda909b18d2eeb893
munabhusal <munabhusal323@gmail.com>
2024-07-11 18:57:00 +0545

    login file with bootstrap in react

--- react/src/assets/views/Login.jsx
+++ react/src/assets/views/Login.jsx
@@ -1,8 +1,33 @@
+import {Link } from "react-router-dom"
 function Login() {
+
+  const onSubmit = (event)=>{
+    event.preventDefault()
+  }
 
     return (
       <div className="Login">
-        Login
+      <div className="container">
+      <form onSubmit={onSubmit}>
+        <h2>Login Form</h2> 
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+
+        
+        <div className="mb-3">
+          <div id="emailHelp" className="form-text">
+          Not Registred? <Link to="/signup">Create an Account</Link>
+          </div>
+        </div>
+      </form>
+      </div>
       </div>
     )
   }
