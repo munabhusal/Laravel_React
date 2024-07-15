91d9707f0f63efc30c06fc390aa7947d7a375a7a
munabhusal <munabhusal323@gmail.com>
2024-07-15 14:48:09 +0545

    Api and Authentication- backend portion

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request){
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($request['password'])
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+
+    }
+    public function login(LoginRequest $request){
+        $credentials = $request->validated();
+
+        if(!Auth::attempt($credentials)){
+            return response([
+                'message'=>"Provided data didn't matched!"
+            ], status:422);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+
+
+    public function logout(Request $request){
+        
+        /** @var User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response('', 204);
+    }
+
+
+}
